d4db9a95a71a1962633f6c49dfae6913dabfa211
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-21 21:23:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function fibonacci(num) {
 // your code here
+	 let a = 0;
+        let b = 1;
+        let c;
+        if(num == 1) {
+            console.log(0);
+        }
+        else if(num == 2) {
+            console.log(1);
+        }
+        else {
+        for(let i = 3;i<=num;i++) {
+            c = a+b;
+            a = b;
+            b = c;
+        }
+            console.log(c);
+        }
+    }
 }
 
 module.exports = fibonacci;
